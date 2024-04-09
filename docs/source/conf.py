ceeef735daa6c14728b5330657a538bb5cf1abd4
Sergey Isaev <serj.hoop@gmail.com>
2024-04-09 15:50:43 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,12 +2,12 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Symphonypy'
+copyright = '2023, Petrova, Isaev'
+authors = 'Petrova, Isaev'
 
-release = '0.1'
-version = '0.1.0'
+release = '0.2.1'
+version = '0.2.1'
 
 # -- General configuration
 
